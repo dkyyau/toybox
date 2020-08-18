0197610f73020e4d426ce43967136c8104d2d95f
David Magueijo <davedev1984@gmail.com>
2020-08-18 16:56:59 +0100

    Added Offer controller methods, fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :toys do
-    resources :offers, only: [ :index, :show, :new, :create, :edit, :update ]
+    resources :offers, only: [ :index, :new, :create, :edit, :update ]
   end
-  resources :offers, only: [ :destroy ]
+  resources :offers, only: [ :show, :destroy ]
   resources :users, only: [ :show ]
 end
