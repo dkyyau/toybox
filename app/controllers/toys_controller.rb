2d45ccb011fb0b707b4a44df20f4157164fd33f8
Shreya Gupta <shreyagupta2906@gmail.com>
2020-08-18 14:05:33 +0100

    done the controller for toys

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,30 +1,46 @@
 class ToysController < ApplicationController
-  # def index
-  # end
+  def index
+    @toys= Toy.all
+  end
 
-  # def show
-  # end
+  def show
+    @toy = Toy.find(params[:id])
+  end
 
-  # def new
-  # end
+   def new
+    @toy = Toy.new
+   end
 
-  # def create
-  # end
+   def create
+    @toy = Toy.new(toy_params)
+    if @toy.save
+      redirect_to toy_path(@toy)
+    else
+      render 'new'
+   end
 
-  # def edit
-  # end
+   def edit
+    @toy = Toy.find(params[:id])
+   end
 
-  # def update
-  # end
+   def update
+    @toy = Toy.find(params[:id])
+    @toy.update(toy_params)
+    redirect_to toy_path(@toy)
+   end
 
-  # def destroy
-  # end
+   def destroy
+    @toy = Toy.find(params[:id])
+    @toy.destroy
+    redirect_to toys_path
+   end
 
-  # private
+   private
 
   # def set_toy
   # end
 
-  # def toy_params
-  # end
+   def toy_params
+    params.require(:toy).permit(:name, :description, :price, :category)
+   end
 end
