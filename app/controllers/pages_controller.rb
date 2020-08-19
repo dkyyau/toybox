44a9abd18f66daccca3929928094fe1d8bf74344
simeo russo <simeo94@hotmail.com>
2020-08-19 11:40:36 +0100

    created profle conroler and empty view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
   end
+
+  def profile
+   @my_toys = current_user.toys
+   @my_offers = current_user.offers
+  end
 end
