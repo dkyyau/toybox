f2f34ca99c54cf74d8479bf42f7cce03eceb17c8
simeo russo <simeo94@hotmail.com>
2020-08-20 20:54:36 +0100

    made the footer smaller in height

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/dkyy/cke2onkgu13xn19npfwez2f7k'
+      style: 'mapbox://styles/mapbox/streets-v11'
     });
    const markers = JSON.parse(mapElement.dataset.markers);
       markers.forEach((marker) => {
