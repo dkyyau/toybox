764996df7ef9f01c8d4e1911d3c5e6760bed4134
Shreya Gupta <shreyagupta2906@gmail.com>
2020-08-21 11:40:31 +0100

    added a colours column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_095222) do
+ActiveRecord::Schema.define(version: 2020_08_21_103924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_095222) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "colour"
   end
 
   create_table "messages", force: :cascade do |t|
